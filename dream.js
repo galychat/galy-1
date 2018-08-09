4385ee709bdeb96cbbea5b049fc1c9a5ccb00650
zain0011 <40404880+zain0011@users.noreply.github.com>
2018-08-10 01:21:55 +0300

    Create dream.js

---
+++ dream.js
@@ -0,0 +1,36 @@
+$(`<center><div style="position: static;width: 100%;" onclick="setTimeout(function(){fixSize();},800);"><div width="99.5%" id="design-a"style="background-color: antiquewhite;color: #105873;font-family: jazeera-light, FontAwesome;border-bottom: 3px solid #105873;border-top: 3px solid #6b5f5f;padding-bottom: 2px;border-radius: 0px 0px 15px 15px;" ><font id="sultan" class=" sultan11"style="
+    font-family: jazeera-light, FontAwesome;
+" >  <u><b>اضغط هنا لمعرفة قوانين الدردشة</b></u> </font><div id="spanan" style="font-family: jazeera-light, FontAwesome;color: red;text-align: right;display:none;" >  
+		<p style="text-align: center"><b><font color="#000080"يرجى الالتزام بالقوانين</font></b>
+		</p>
+</p>
+<div style="float: right;padding-right: 20px;">- 1</div>  ( 
+		الالتزام بالدخول بالأسماء المحترمة وتجنب الأسماء التي تحمل أي إيحاءات
+ <img src=""> 
+</p>
+<div style="float: right;padding-right: 20px;">- 2</div>  ( 
+		عدم تكرار الجمل والابتسامات اكتر من ثلاث مرات على العام لتجنب مضايقة أعضاء الدردشة
+ <img src="">
+</p>
+<div style="float: right;padding-right: 20px;">- 3</div> 
+		احترام جميع الموجودين في الدردشة وعدم السخرية أو التهكم على أي شخص مهما كان
+ <img src=""> 
+</p>
+<div style="float: right;padding-right: 20px;">- 4</div>  
+		عدم الخوض في مواضيع تمس الديانات السماوية
+ <img src=""> 
+</p>
+<div style="float: right;padding-right: 20px;">- 5</div>  
+في حالة طردك من احد السوابر بدون سبب يرجى عدم فتح الموضوع في العام ومراسلة الاداره في الخاص فقط مع اخذ سكرينات <img src=""> 
+</p>
+<div style="float: right;padding-right: 20px;">- 6</div>  
+		ممنوع استفزاز ومضايقه الأعضاء على العام واذ حذث هذا فى الخاص يمكنك تجاهله
+ <img src=""> 
+ 	
+
+		<p>
+		<b>
+		<font color="#0000FF">احظة : عند تعرضك مضايقة من احد السوابر او الاعضاء برجى مراسلة الاداره [زين] او [ازار]ـ</font></b><p style="text-align: center">
+		<font color="#800000"><u><b>
+		<a target="_blank" href="https://galychat.com/"></a></b></u></font></div> </div></div><center>`).insertBefore('#d2');
+    $("#design-a").click(function(){$("#spanan").slideToggle(function(){});});
