40f847459d4e644ecaa26a6770311bcd8660bf21
zain0011 <40404880+zain0011@users.noreply.github.com>
2018-08-10 20:42:33 +0300

    Create dok.js

---
+++ dok.js
@@ -0,0 +1,27 @@
+
+if(location.protocol!='https:'){location.protocol='https:';}
+$('.break.light').css({"background-image":"url(http://alashaaaq.com/sico/z1ch6nnbcn10.PNG)"})
+$(`<img src="https://galychat.com/sico/z1cki0rftu10.gif" class="fr" style="width:100%;margin-top: 1px;">
+<div style="text-align:center;background: #999999;">
+<a href="http://ala"><img src=https://www.alashaaaq.com/sico/z1ch6nm41j10.PNG?v=2018" height="25" width="85" alt="شات"></a>
+<a target="_blank" href="sub.html">
+<img src=https://www.alashaaaq.com/sico/z1cia072mv10.PNG?v=2018" height="25" width="85"></a>
+<a href="https://play.google.com/store/apps/details?id=com.w_5702481"><img src=https://www.alashaaaq.com/sico/z1ch6nlu4b10.PNG?v=2018" height="25" width="85"></a>
+<a href="javascript:void(0);" Onclick="ShowHide5();"><img src=https://www.alashaaaq.com/sico/z1ch6nlocl10.PNG?v=2018" height="25" width="85"></a>
+</div><div style="text-align:center;color: #fff; padding: 2px; background: #7B1B11; max-width: 290px; border-radius: 12px; text-align: center; margin-right: auto; margin-left: auto; font-size:25px;  display: none;" name="help" ID="help5">
+ للتواصل مع الاداره على التيليكرام<br><a href="mailto:zain@gmail.com">Telegrame</a>
+<br>زيـــن [@zain01]<br>ازار [galychat]<br>
+<a title="" href="/"></a>,<a title="غالي وغالي" href="/">دردشة غالي</a>,<a title="شات غالي" href="/">شات غالي وغالي</a></div>
+<center>
+<img src=https://galychat.com/sico/z1cjebvv3d10.gif  WIDTH=250 HEIGHT=35><br>(&#127801;<font size="3">مرحباً بكم في شات غالي</font>)&#127801;
+<marquee direction="Up" width="100%" height="100" scrollAmount="1" align="center" style="border:#7B1B11 solid 2px;"><font size="2" color="#750000"><center>
+<br>نجــــوم شات غالي وغالي للجــــــوال
+<br><br><img src="https://www.alashaaaq.com/sico/z1cjgr6kk810.gif?v=2018" alt="نجم الموقع" height="30" width="100">
+<br><br>(&#127801;<img src="https://galychat.com/sico/z1cjqoasgh10.gif?v=2018" alt="صورة نجم الموقع" height="30" width="100">&#127801;)
+<br><br><img src="https://www.alashaaaq.com/sico/z1chlsuof110.gif?v=2018" alt="ملكة الحائط" height="30" width="100">
+<br><br>(&#127801;<img src="https://galychat.com/sico/z1ckco1frp10.gif?v=2018" alt="صورة ملك الحائط" height="30" width="100">&#127801;)
+<br><br><img src="https://www.alashaaaq.com/sico/z1cjgr7acj10.gif?v=2018" alt="افضل تواجد" height="30" width="100">
+<br><br>(&#127801;<img src="https://galychat.com/sico/z1ckff5sui10.gif?v=2018" alt="صورة افضل تواجد" height="30" width="100">&#127801;)
+<br><br><img src="https://www.alashaaaq.com/sico/z1cjgr6t6t10.gif?v=2018" alt="افضل سوبر" height="30" width="100">
+<br><br>(&#127801;<img src="https://galychat.com/sico/z1ckd9ifpe10.gif?v=2018" alt="صورة افضل سوبر" height="30" width="100">&#127801;)
+<br>لكـــم تحيات ادارة شات غالي وغالي للجوال</font></marquee>
